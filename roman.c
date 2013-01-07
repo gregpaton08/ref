d163e3c1102dd18b3fd43920caf0674a3f53acc2
gregpaton08 <gregpaton08@gmail.com>
2013-01-06 23:28:12 -0500

    [mod] updated roman.c

--- roman.c
+++ roman.c
@@ -13,31 +13,47 @@
 
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// arrays to hold roman numerals and sizes
+const char *huns[] = {"", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"};
+const char *tens[] = {"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"};
+const char *ones[] = {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"};
+const int   size[] = { 0,   1,    2,     3,    2,   1,    2,     3,      4,    2};
+
+// converts intger to roman numeral
+void int_to_roman(int val, char *res) {
+    
+    while (val >= 1000) {
+        sprintf(res, "M");
+        *res++;
+        val -= 1000;
+    }
+    
+    strcpy(res, huns[val/100]);
+    res += size[val/100];
+    val = val % 100;
+    strcpy(res, tens[val/10]);
+    res += size[val/10];
+    val = val % 10;
+    strcpy(res, ones[val]);
+    res += size[val];
+    *res = 0;
+}
 
 int main(int argc, char **argv) {
     int val;
-    while (val >= 1000) {
-        printf("M");
-        val -= 1000;
+    char res[128];
+    
+    if (argc == 2)
+        val = atoi(argv[1]);
+    else {
+        printf("usage: %s number_to_convert\n", argv[0]);
+        return 0;
     }
-    while (val >= 500) {
-        printf("D");
-        val -= 500;
-    }
-    while (val >= 100) {
-        printf("C");
-        val -= 100;
-    }
-    while (val >= 50) {
-        printf("L");
-        val -= 50;
-    }
-    while (val >= 10) {
-        printf("X");
-        val -=10;
-    }
-    while (val >= 5) {
-        printf("V");
-        val -= 5;
-    }
+    
+    int_to_roman(val, res);
+    
+    printf("%s\n", res);
 }
